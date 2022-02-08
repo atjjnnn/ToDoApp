100ef812c7a88e91a40e6ede4f3c218aca91fbba
atjjnnn <50092173+atjjnnn@users.noreply.github.com>
2022-02-09 00:02:58 +0900

    add: add discriptions for some functions

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+/* htmlからidで要素取得 */
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 
+/* イベントが起きた時の処理 = ここではsubmit(エンター)された時の処理 */
 form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    console.log(input.value);
+    event.preventDefault();   //フォームをsubmitした時のブラウザのリロードの中断
+    console.log(input.value); //入力された文字や数値の出力
 })
