b1d684196dd30ab35ed238f1d22de1ba436a41d8
atjjnnn <50092173+atjjnnn@users.noreply.github.com>
2022-02-09 00:17:00 +0900

    add: add a function which creates a to-do list

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 /* htmlからidで要素取得 */
 const form = document.getElementById("form");
 const input = document.getElementById("input");
+const ul = document.getElementById("ul");
 
 /* イベントが起きた時の処理 = ここではsubmit(エンター)された時の処理 */
 form.addEventListener("submit", function (event) {
     event.preventDefault();   //フォームをsubmitした時のブラウザのリロードの中断
-    console.log(input.value); //入力された文字や数値の出力
-})
+    add();
+});
+
+function add() {
+    const li = document.createElement("li");
+    li.innerText = input.value;
+    li.classList.add("list-group-item");
+    ul.appendChild(li);
+    input.value = "";
+}
