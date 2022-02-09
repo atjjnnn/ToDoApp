2cd5a8f83d4cecbcbd279949be80eeaeb637c959
atjjnnn <50092173+atjjnnn@users.noreply.github.com>
2022-02-09 19:51:28 +0900

    add: add some comments for function

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const ul = document.getElementById("ul");
 
 /* イベントが起きた時の処理 = ここではsubmit(エンター)された時の処理 */
 form.addEventListener("submit", function (event) {
-    event.preventDefault();   //フォームをsubmitした時のブラウザのリロードの中断
+    /* フォームをsubmitした時のブラウザのリロードの中断 */
+    event.preventDefault();
+    /* エンターを押されたらリストを追加する */
     add();
 });
 
+/* ユーザがフォームに入力したテキストをliタグとして画面に追加する */
 function add() {
+    /* liタグを作る */
     const li = document.createElement("li");
+    /* liをテキストとしてユーザが入力した値を入れる */
     li.innerText = input.value;
+    /* liタグにデザインを当てる */
     li.classList.add("list-group-item");
+    /* ulタグの子供にliタグを追加する */
     ul.appendChild(li);
+    /* 入力フォームを空にする */
     input.value = "";
 }
